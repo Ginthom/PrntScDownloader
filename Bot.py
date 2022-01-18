669cb76f4a35b41435b2ffcea1fdf234ca26a11c
Thomas Gingele <thomas@gingele.de>
2022-01-18 18:10:33 +0100

    Implemeted multiple pictures at once

--- Bot.py
+++ Bot.py
@@ -9,9 +9,27 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content == ("./pic"):
-        # get image link
-        image_link = get_random_link()
+    if message.content.startswith("./pic"):
+        split_msg = message.content.split(" ")
+        amount = 1
+        image_link = ""
+
+        if len(split_msg) == 2:
+            try:
+                amount = int(split_msg[1])
+
+            except:
+                message.channel.send("Make sure your parameter is a number")
+
+        if amount > 5: 
+            amount = 5
+        elif amount < 1:
+            amount = 1
+
+        print(amount)
+        for i in range(amount):
+            # get image link
+            image_link += get_random_link() + "\n"
 
         # reply with  image link
         await message.channel.send(image_link)
