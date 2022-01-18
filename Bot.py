79df511d15b2cf4371d8e664afd20c4ad84d2230
Thomas Gingele <thomas@gingele.de>
2022-01-18 17:24:28 +0100

    ...

--- Bot.py
+++ Bot.py
@@ -1,3 +1,19 @@
 from discord import *
+from psd import *
 
 client = discord.Client()
+
+@client.event 
+async def on_ready():
+    print("Hello everyone. I have beeen booted into random screenshot mode.")
+
+@client.event
+async def on_message(message):
+    if message.content == ("./pic"):
+        # get image link
+        image_link = get_random_link
+
+        # reply with  image link
+        await client.send_message(message.channel, image_link)
+
+client.run('')
