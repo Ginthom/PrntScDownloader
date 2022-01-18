3788283914bf9d3f97b2563c23dcebdc27d43661
Thomas Gingele <thomas@gingele.de>
2022-01-18 16:24:09 +0100

    Remove iterating_downloader.py

--- psd.py
+++ psd.py
@@ -5,8 +5,6 @@ from iterating_downloader import iterating_download
 
 try:
     download_ammount = int(sys.argv[1])
-    download_order = sys.argv[2]
-    first_id = sys.argv[3] 
 
 except:
     print("There was an error with the command line arguments!\n"+
@@ -14,8 +12,5 @@ except:
           "python psd.py <ammount to download> <i/r: download order> <the first it to download>")
     exit(-1)
 
-if download_order == 'r':
-    random_download(download_ammount)
-elif download_order == 'i':
-    iterating_download(download_ammount, first_id)
+random_download(download_ammount)
 
