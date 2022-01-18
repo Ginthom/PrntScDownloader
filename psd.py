7dceab8e37e0d111026717e072ed504c1284b49f
Thomas Gingele <thomas@gingele.de>
2022-01-18 17:11:20 +0100

    Did some stuff

--- psd.py
+++ psd.py
@@ -1,16 +1,55 @@
 import sys
+import argparse
+import random
+import urllib3
+import os
 
-from random_downloader import random_download
-from iterating_downloader import iterating_download
+static_link = "https://prnt.sc/"
 
-try:
-    download_ammount = int(sys.argv[1])
+letter_sequence = "abcdefghijklmnopqrstuvwxyz"
+number_sequence = "1234567890"
 
-except:
-    print("There was an error with the command line arguments!\n"+
-          "Please keep to the following format:\n"+
-          "python psd.py <ammount to download> <i/r: download order> <the first it to download>")
-    exit(-1)
+letter_count = 2
+number_count = 4
 
-random_download(download_ammount)
+def random_download(download_ammount):
+    # Keep downloading until app is canceled
+    for ammount_to_download in range(download_ammount):
+        imageId = ""
 
+        # Create letters for Id
+        for i in range(letter_count):
+            imageId += random.choice(letter_sequence)
+
+        # Create number for Id
+        for i in range(number_count):
+            imageId += random.choice(number_sequence)
+
+        grab_image(imageId)
+
+
+def grab_image(id):
+    dynamic_link = static_link + id
+
+    http = urllib3.PoolManager()
+    result = http.request('GET', dynamic_link)
+    test = str(result.data)
+
+    index = test.find("https://image.prntscr.com/image/")
+    print(index)
+    image_name_end_index = test[index : index + 100].find(".png") + 4
+    print(image_name_end_index)
+    image_url = test[index : index + image_name_end_index]
+    print(image_url)
+
+    os.system("wget --output-document=./Downloads/image_" + id + ".png " + image_url)
+
+
+parser = argparse.ArgumentParser(description="Download random screenshots from prnt.sc")
+parser.add_argument("-a", "--ammount", help="Set the ammountof images you want to download", nargs="+")
+args = parser.parse_args()
+
+if args.ammount:
+    random_download(int(args.ammount[0]))
+else:
+    print("Type -h or --help for instructions")
--- values.py
+++
@@ -1,8 +0,0 @@
-# Constants
-static_link = "https://prnt.sc/"
-
-letter_sequence = "abcdefghijklmnopqrstuvwxyz"
-number_sequence = "1234567890"
-
-letter_count = 2
-number_count = 4
